503dedc4b53eb54ece71df576af746a9e385dc7e
CFD Friend <cfdfriend@users.sourceforge.net>
2012-04-09 11:35:16 +0200

    added solver for steady combusting flow: edcSimpleFoam including example

---
+++ applications/edcFoam/edcFoam.C
@@ -0,0 +1,114 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Application
+
+Description
+    the transport velocity is the same as that for the Xi equation.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "hCombustionThermo.H"
+#include "turbulenceModel.H"
+#include "ignition.H"
+#include "IFstream.H"
+#include "OFstream.H"
+
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+
+#   include "setRootCase.H"
+
+#   include "createTime.H"
+#   include "createMeshNoClear.H"
+#   include "readGravitationalAcceleration.H"
+#   include "createFields.H"
+#   include "readPISOControls.H"
+#   include "initContinuityErrs.H"
+//#   include "readIgnitionProperties.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    for (runTime++; !runTime.end(); runTime++)
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+#       include "compressibleCourantNo.H"
+
+#       include "rhoEqn.H"
+
+        turbulence->correct();
+
+#       include "UEqn.H"
+
+        // --- PISO loop
+        for (int corr=1; corr<=nCorr; corr++)
+        {
+
+            tmp<fv::convectionScheme<scalar> > mvConvection
+                (
+                    fv::convectionScheme<scalar>::New
+                    (
+                        mesh,
+                        fields,
+                        phi,
+                        mesh.divScheme("div(phi,b_ft_h)")
+                    )
+                );
+
+#           include "ftEqn.H"
+
+#           include "bEqn.H"
+
+#           include "hEqn.H"
+
+            thermo.correct();
+
+#           include "pEqn.H"
+        }
+
+
+        runTime.write();
+
+        rho = thermo.rho();
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return(0);
+}
+
+
+// ************************************************************************* //
--- bashrc
+++ bashrc
@@ -7,3 +7,6 @@ CUBA_PATH=$HOME/Programme/Cuba-3.0/platforms/$WM_OPTIONS
 export CUBA_INC_PATH=$CUBA_PATH/include
 export CUBA_LIB_PATH=$CUBA_PATH/lib
 
+CANTERA_PATH=/usr/local/cantera
+export CANTERA_INC_PATH=$CANTERA_PATH/include
+export CANTERA_LIB_PATH=$CANTERA_PATH/lib
